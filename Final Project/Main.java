3c28fbf6d835355d832b5856dc3b334911d45b01
JalaAguirre <127201705+JalaAguirre@users.noreply.github.com>
2023-12-18 01:05:57 +0800

    Update Main.java
    Placed the AnimalActions into a separate class file

--- Final Project/Main.java
+++ Final Project/Main.java
@@ -3,179 +3,70 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         AnimalCatalogueSystem catalogueSystem = new AnimalCatalogueSystem();
+        AnimalMenu animalMenu = new AnimalMenu(catalogueSystem);
+
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("1. Report Lost/Found Animal");
-            System.out.println("2. Adopt an Animal");
-            System.out.println("3. Search for Animals");
-            System.out.println("4. Update Animal Record");
-            System.out.println("5. Delete Animal Record");
-            System.out.println("6. Exit");
-
-            System.out.print("Enter your choice: ");
+            animalMenu.displayMenu();
             int choice = scanner.nextInt();
             scanner.nextLine();
-
-            switch (choice) {
-                case 1:
-                    reportLostFoundAnimal(catalogueSystem, scanner);
-                    break;
-                case 2:
-                    adoptAnimal(catalogueSystem, scanner);
-                    break;
-                case 3:
-                    searchForAnimals(catalogueSystem, scanner);
-                    break;
-                case 4:
-                    updateAnimalRecord(catalogueSystem, scanner);
-                    break;
-                case 5:
-                    deleteAnimalRecord(catalogueSystem, scanner);
-                    break;
-                case 6:
-                    catalogueSystem.saveToCSV(); // Save data to CSV before exiting
-                    System.out.println("Exiting...");
-                    System.exit(0);
-                default:
-                    System.out.println("Invalid choice. Please try again.");
-            }
-        }
-    }
-
-    private static void reportLostFoundAnimal(AnimalCatalogueSystem catalogueSystem, Scanner scanner) {
-        System.out.print("Enter animal type (Dog/Cat): ");
-        String animalType = scanner.nextLine();
-    
-        System.out.print("Enter animal name: ");
-        String name = scanner.nextLine();
-    
-        System.out.print("Enter animal breed: ");
-        String breed = scanner.nextLine();
-
-        System.out.print("Enter animal color: ");
-        String color = scanner.nextLine();
-    
-        System.out.print("Enter health status: ");
-        String healthStatus = scanner.nextLine();
-    
-        System.out.print("Enter age: ");
-        int age = scanner.nextInt();
-        scanner.nextLine(); 
-    
-        System.out.print("Is the animal missing? (true/false): ");
-        boolean isLost = scanner.nextBoolean();
-        scanner.nextLine(); 
-    
-        Animal animal;
-        if ("Dog".equalsIgnoreCase(animalType)) {
-            animal = new Dog(name, breed, color, healthStatus, age, isLost);
-        } else if ("Cat".equalsIgnoreCase(animalType)) {
-            animal = new Cat(name, breed, color, healthStatus, age, isLost);
-        } else {
-            System.out.println("Invalid animal type. Animal not reported.");
-            return;
-        }
-    
-        catalogueSystem.reportLostFoundAnimal(animal, isLost);
-    }
-
-    private static void adoptAnimal(AnimalCatalogueSystem catalogueSystem, Scanner scanner) {
-        //System.out.println("Available animals for adoption:");
-        catalogueSystem.displayAvailableAnimals();
-    
-        System.out.print("Enter the name of the animal you want to adopt: ");
-        String adoptAnimalName = scanner.nextLine();
-    
-        Animal adoptAnimal = catalogueSystem.getAnimalByName(adoptAnimalName);
-    
-        if (adoptAnimal != null) {
-            catalogueSystem.adoptAnimal(adoptAnimal);
-            // if (adoptAnimal instanceof Dog) {
-            //     System.out.println("Adoption Fee: $" + ((Dog) adoptAnimal).calculateAdoptionFee());
-            // } else if (adoptAnimal instanceof Cat) {
-            //     System.out.println("Adoption Fee: $" + ((Cat) adoptAnimal).calculateAdoptionFee());
-            // }
-        } else {
-            System.out.println("Animal not found for adoption.");
-        }
-    }
-    
-    
-    private static void searchForAnimals(AnimalCatalogueSystem catalogueSystem, Scanner scanner) {
-        System.out.print("Enter Breed: ");
-        String breed = scanner.nextLine();
-
-        catalogueSystem.searchForAnimals(breed);
-    }
-
-    private static void updateAnimalRecord(AnimalCatalogueSystem catalogueSystem, Scanner scanner) {
-        System.out.println("Available animals for updating:");
-        catalogueSystem.displayAvailableAnimals();
-    
-        System.out.print("Enter the name of the animal you want to update: ");
-        String updateAnimalName = scanner.nextLine();
-    
-        Animal oldAnimal = catalogueSystem.getAnimalByName(updateAnimalName);
-    
-        if (oldAnimal != null) {
-            System.out.println("Enter updated information for the animal:");
-    
-            System.out.print("Enter animal type (Dog/Cat): ");
-            String animalType = scanner.nextLine();
-    
-            System.out.print("Enter animal name: ");
-            String name = scanner.nextLine();
-    
-            System.out.print("Enter animal breed: ");
-            String breed = scanner.nextLine();
-
-            System.out.print("Enter animal color: ");
-            String color = scanner.nextLine();
-    
-            System.out.print("Enter health status: ");
-            String healthStatus = scanner.nextLine();
-    
-            System.out.print("Enter age: ");
-            int age = scanner.nextInt();
-            scanner.nextLine(); 
-    
-            System.out.print("Is the animal lost? (true/false): ");
-            boolean isLost = scanner.nextBoolean();
-            scanner.nextLine(); 
-    
-            Animal newAnimal;
-            if ("Dog".equalsIgnoreCase(animalType)) {
-                newAnimal = new Dog(name, breed, color, healthStatus, age, isLost);
-            } else if ("Cat".equalsIgnoreCase(animalType)) {
-                newAnimal = new Cat(name, breed, color, healthStatus, age, isLost);
-            } else {
-                System.out.println("Invalid animal type. Animal not updated.");
-                return;
-            }
-    
-            catalogueSystem.updateAnimal(oldAnimal, newAnimal);
-        } else {
-            System.out.println("Animal not found for updating.");
-        }
-    }
-    
-    
-
-    private static void deleteAnimalRecord(AnimalCatalogueSystem catalogueSystem, Scanner scanner) {
-        System.out.println("Available animals for deletion:");
-        catalogueSystem.displayAvailableAnimals();
-
-        System.out.print("Enter the name of the animal you want to delete: ");
-        String deleteAnimalName = scanner.nextLine();
-
-        
-        Animal deleteAnimal = catalogueSystem.getAnimalByName(deleteAnimalName);
-
-        if (deleteAnimal != null) {
-            catalogueSystem.deleteAnimal(deleteAnimal);
-        } else {
-            System.out.println("Animal not found for deletion.");
+            animalMenu.handleMenuChoice(choice, scanner);
         }
     }
 }
+
+class AnimalMenu {
+    private AnimalCatalogueSystem catalogueSystem;
+
+    public AnimalMenu(AnimalCatalogueSystem catalogueSystem) {
+        this.catalogueSystem = catalogueSystem;
+    }
+
+    public void displayMenu() {
+        System.out.println("============-= ^._.^ =-===========");
+        System.out.println("== Animal Catalogue System Menu ==");
+        System.out.println("==============U ^ v ^ U=============\n");
+
+        System.out.println("Welcome! How may we help you today? \n");
+
+        System.out.println("1. Report Lost/Found Animal");
+        System.out.println("2. Adopt an Animal");
+        System.out.println("3. Search for Animals");
+        System.out.println("4. Update Animal Record");
+        System.out.println("5. Delete Animal Record");
+        System.out.println("6. Exit");
+
+        System.out.print("Enter your choice: ");
+    }
+
+    public void handleMenuChoice(int choice, Scanner scanner) {
+        switch (choice) {
+            case 1:
+                AnimalActions.reportLostFoundAnimal(catalogueSystem, scanner);
+                break;
+            case 2:
+                AnimalActions.adoptAnimal(catalogueSystem, scanner);
+                break;
+            case 3:
+                AnimalActions.searchForAnimals(catalogueSystem, scanner);
+                break;
+            case 4:
+                AnimalActions.updateAnimalRecord(catalogueSystem, scanner);
+                break;
+            case 5:
+                AnimalActions.deleteAnimalRecord(catalogueSystem, scanner);
+                break;
+            case 6:
+                System.out.println("-------------------(^o^)/-----------------");
+                System.out.println("Exiting Animal Catalogue System. Goodbye!");
+                System.out.println("-------------------------------------------");
+                System.exit(0);
+            default:
+                System.out.println("--------------------(😑)--------------------");
+                System.out.println("Invalid choice. Please enter a valid option.");
+                System.out.println("---------------------------------------------");
+        }
+    }
+}
+
